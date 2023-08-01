b9ac26d4d8e02be9afb7f27dd548ac42bb29f0cc
konjoyeww <kemal.siraj@a2sv.org>
2023-08-01 14:58:31 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,16 +1,9 @@
 const _ = require('lodash');
-// Create an array of numbers
-const numbers = [1, 2, 3, 4, 5];
-
-// Use Lodash to get the sum of the numbers
-const sum = _.sum(numbers);
-console.log(sum); // Output: 15
-
-// Use Lodash to get the average of the numbers
-const average = _.mean(numbers);
-console.log(average); // Output: 3
-
-// Use Lodash to find the maximum and minimum values in the array
-const max = _.max(numbers);
-const min = _.min(numbers);
-console.log(max, min); // Output: 5 1
+//fibonace
+ function fibonace(n){
+    if (n==0) return 0;
+    if (n==1) return 1;
+    return fibonace(n-1)+fibonace(n-2);
+ }
+ fibonace=_.memoize(fibonace);
+ console.log(fibonace(6))
